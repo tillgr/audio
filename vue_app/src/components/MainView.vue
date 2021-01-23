d67300bd7fc82dbbd113cb478979f28b21a809f2
Till Große <till.grosse@web.de>
2021-01-23 18:25:16 +0100

    adjust zoom

--- vue_app/src/components/MainView.vue
+++ vue_app/src/components/MainView.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="MainView"  @keyup="move('up')">
+  <div class="MainView" @keyup="move('up')">
     <ZoomControls
-        v-on:zoom-in = "zoom(0.2)"
-        v-on:zoom-out = "zoom(-1/3)"
-        v-on:move-up = "move('up')"
-        v-on:move-down = "move('down')"
-        v-on:move-left = "move('left')"
-        v-on:move-right = "move('right')">
+        v-on:zoom-in="zoomIn()"
+        v-on:zoom-out="zoomOut()"
+        v-on:move-up="move('up')"
+        v-on:move-down="move('down')"
+        v-on:move-left="move('left')"
+        v-on:move-right="move('right')">
     </ZoomControls>
-    <div id= "graph" class="d3js-container" ></div>
+    <div id="graph" class="d3js-container"></div>
   </div>
 </template>
 
@@ -17,9 +17,10 @@ import ZoomControls from "@/components/ZoomControls";
 import soundData from "../../../AudioKPPython/coord/completeDict.json"
 import * as d3 from "d3";
 
+let zoomFactor = 1;
 export default {
   name: "MainView",
-  components:{
+  components: {
     ZoomControls
   },
   data() {
@@ -35,25 +36,39 @@ export default {
   methods: {
     init() {
       this.id = "graph";
-      let xMinMax = d3.extent(this.dataSet.reduce(function(a,b) { return a.concat(b.x) }, []));
-      let yMinMax = d3.extent(this.dataSet.reduce(function(a,b) { return a.concat(b.y) }, []));
-      this.visibleCoordinates = { xMin: xMinMax[0] - 1,
-                                  xMax: xMinMax[1] + 1,
-                                  yMin: yMinMax[0] - 1,
-                                  yMax: yMinMax[1] + 1};
+      let xMinMax = d3.extent(this.dataSet.reduce(function (a, b) {
+        return a.concat(b.x)
+      }, []));
+      let yMinMax = d3.extent(this.dataSet.reduce(function (a, b) {
+        return a.concat(b.y)
+      }, []));
+      this.visibleCoordinates = {
+        xMin: xMinMax[0] - 1,
+        xMax: xMinMax[1] + 1,
+        yMin: yMinMax[0] - 1,
+        yMax: yMinMax[1] + 1
+      };
       this.startingDimensions = this.visibleCoordinates;
     },
     drawGlyphGraph() {
       // create svg object of the graph in the correct DOM Object (this.id)
       // TODO get graph to fill whole height
-      this.graphDimensions = {width: document.getElementById("graph").clientWidth,
-        height: document.getElementById("graph").clientHeight}
+      this.graphDimensions = {
+        width: document.getElementById("graph").clientWidth,
+        height: document.getElementById("graph").clientHeight
+      }
       console.log(this.graphDimensions.width, this.graphDimensions.height);
+
+      /*.attr('width', this.graphDimensions.width)
+      .attr('height', this.graphDimensions.height)*/
 
       const svg = d3.select(`#${this.id}`)
           .append('svg')
+          .attr('viewBox', '0, 0,' + this.graphDimensions.width + ', ' + this.graphDimensions.height)
           .attr('width', this.graphDimensions.width)
           .attr('height', this.graphDimensions.height)
+          /*.attr('width', 100)
+          .attr('height', 100)*/
           .attr('position', 'absolute');
       // scales
       const xScale = d3.scaleLinear()
@@ -75,45 +90,47 @@ export default {
       var glyphs = graph.selectAll('g').data(this.dataSet).enter();
 
       glyphs.append('circle')
-            .attr('class', 'innerGlyphCircle')
-            .attr('r', `${glyphRadius}px`)
-            .attr('cx', d => xScale(d.x) )
-            .attr('cy', d => yScale(d.y) )
-            .attr('fill', d => this.circleAttr(d).innerFill)
-            .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray )
-            .attr('stroke-width', `${glyphRadius / 15.0}px`)
-            .attr('stroke', 'black')
-            .attr('visibility', d => this.circleAttr(d).inner);
+          .attr('class', 'innerGlyphCircle')
+          .attr('r', `${glyphRadius}px`)
+          .attr('cx', d => xScale(d.x))
+          .attr('cy', d => yScale(d.y))
+          .attr('fill', d => this.circleAttr(d).innerFill)
+          .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray)
+          .attr('stroke-width', `${glyphRadius / 15.0}px`)
+          .attr('stroke', 'black')
+          .attr('visibility', d => this.circleAttr(d).inner);
 
       glyphs.append('circle')
-            .attr('class', 'middleGlyphCircle')
-            .attr('r', `${glyphRadius * 1.15}px`)
-            .attr('cx', d => xScale(d.x) )
-            .attr('cy', d => yScale(d.y) )
-            .attr('fill', 'none')
-            .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray )
-            .attr('stroke-width', `${glyphRadius / 15.0}px`)
-            .attr('stroke', 'black')
-            .attr('visibility', d => this.circleAttr(d).middle);
+          .attr('class', 'middleGlyphCircle')
+          .attr('r', `${glyphRadius * 1.15}px`)
+          .attr('cx', d => xScale(d.x))
+          .attr('cy', d => yScale(d.y))
+          .attr('fill', 'none')
+          .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray)
+          .attr('stroke-width', `${glyphRadius / 15.0}px`)
+          .attr('stroke', 'black')
+          .attr('visibility', d => this.circleAttr(d).middle);
       glyphs.append('circle')
-            .attr('class', 'outerGlyphCircle')
-            .attr('r', `${glyphRadius * 1.3}px`)
-            .attr('cx', d => xScale(d.x) )
-            .attr('cy', d => yScale(d.y) )
-            .attr('fill', 'none')
-            .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray )
-            .attr('stroke-width', `${glyphRadius / 15.0}px`)
-            .attr('stroke', 'black')
-            .attr('visibility', d => this.circleAttr(d).outer);
+          .attr('class', 'outerGlyphCircle')
+          .attr('r', `${glyphRadius * 1.3}px`)
+          .attr('cx', d => xScale(d.x))
+          .attr('cy', d => yScale(d.y))
+          .attr('fill', 'none')
+          .attr('stroke-dasharray', d => this.circleAttr(d).strokeDasharray)
+          .attr('stroke-width', `${glyphRadius / 15.0}px`)
+          .attr('stroke', 'black')
+          .attr('visibility', d => this.circleAttr(d).outer);
     },
     circleAttr(dataPoint) {
-      let attr = {inner: 'hidden',
-                  middle: 'hidden',
-                  outer: 'hidden',
-                  innerFill: 'none',
-                  strokeDasharray: '5, 0'}
+      let attr = {
+        inner: 'hidden',
+        middle: 'hidden',
+        outer: 'hidden',
+        innerFill: 'none',
+        strokeDasharray: '5, 0'
+      }
       // loudness
-      if (dataPoint.loudness > 0.0){
+      if (dataPoint.loudness > 0.0) {
         attr.inner = 'visible';
       }
       if (dataPoint.loudness >= 0.33) {
@@ -141,15 +158,15 @@ export default {
       canvas.removeChild(canvas.childNodes[0]);
       this.drawGlyphGraph();
     },
-    zoom(direction) {
+    /*zoom(direction) {
       let  diffX = (this.visibleCoordinates.xMax - this.visibleCoordinates.xMin)  * direction;
       let  diffY = (this.visibleCoordinates.yMax - this.visibleCoordinates.yMin)  * direction;
       this.visibleCoordinates = { xMin: this.visibleCoordinates.xMin + diffX,
                                   xMax: this.visibleCoordinates.xMax - diffX,
                                   yMin: this.visibleCoordinates.yMin + diffY,
                                   yMax: this.visibleCoordinates.yMax - diffY };
-      /*this.graphDimensions.width += this.graphDimensions.width * direction;
-      this.graphDimensions.height += this.graphDimensions.height * direction;*/
+      /!*this.graphDimensions.width += this.graphDimensions.width * direction;
+      this.graphDimensions.height += this.graphDimensions.height * direction;*!/
       this.redrawGlyphGraph();
     },
     move(directionString) {
@@ -175,31 +192,84 @@ export default {
           break;
       }
       this.redrawGlyphGraph();
+    }*/
+    zoomIn() {
+      zoomFactor = zoomFactor + 0.5;
+      let svg = document.getElementsByTagName('svg')[1];
+      let containerSVG = document.getElementsByClassName('d3js-container')[0];
+      let scrollY = containerSVG.scrollTop / containerSVG.scrollHeight;
+      let scrollX = containerSVG.scrollLeft / containerSVG.scrollWidth;
+
+      console.log('=== adjust scroll ===')
+      console.log('top: '+ containerSVG.scrollTop);
+      console.log('height: '+ containerSVG.scrollHeight);
+
+      svg.style.transformOrigin = 'top left';
+      svg.style.transform = 'scale(' + zoomFactor + ',' + zoomFactor + ')';
+
+
+      containerSVG.scrollTop = scrollY*containerSVG.scrollHeight;
+      containerSVG.scrollLeft = scrollX*containerSVG.scrollWidth;
+
+      console.log('top: '+ containerSVG.scrollTop);
+      console.log('height: '+ containerSVG.scrollHeight);
+    },
+    zoomOut(){
+      zoomFactor = zoomFactor - 0.5;
+      let svg = document.getElementsByTagName('svg')[1];
+      let containerSVG = document.getElementsByClassName('d3js-container')[0];
+      let scrollY = containerSVG.scrollTop / containerSVG.scrollHeight;
+      let scrollX = containerSVG.scrollLeft / containerSVG.scrollWidth;
+
+      console.log('=== adjust scroll ===')
+      console.log('top: '+ containerSVG.scrollTop);
+      console.log('height: '+ containerSVG.scrollHeight);
+
+      svg.style.transformOrigin = 'top left';
+      svg.style.transform = 'scale(' + zoomFactor + ',' + zoomFactor + ')';
+
+
+      containerSVG.scrollTop = scrollY*containerSVG.scrollHeight;
+      containerSVG.scrollLeft = scrollX*containerSVG.scrollWidth;
+
+      console.log('top: '+ containerSVG.scrollTop);
+      console.log('height: '+ containerSVG.scrollHeight);
+
     }
   },
   mounted() {
     this.init();
     this.drawGlyphGraph();
-    }
+  }
 }
 
 </script>
 
 <style scoped>
-  .MainView{
-    width: 86%;
-    height: available;
-    right: 0;
-  }
-  .zoomControls{
-    float: right;
-    margin-right: 0.5rem;
-    margin-top: 0.5rem;
-  }
-  .d3js-container{
-    width: 100%;
-    height: 100%;
-    display: block;
-    background-color: #911F82;
-  }
+.MainView {
+  width: 86%;
+  height: available;
+  right: 0;
+}
+
+.zoomControls {
+  /*float: right;*/
+  /*margin-right: 0.5rem;
+  margin-top: 0.5rem;*/
+  z-index: 3;
+  position: absolute;
+  right: 2vw;
+  top: 4vw;
+}
+
+.d3js-container {
+  width: 100%;
+  height: 100%;
+  display: block;
+  /*background-color: #911F82;*/
+  align-content: start;
+  justify-content: start;
+  overflow: scroll;
+}
+
 </style>
