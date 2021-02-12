038966ae14c8ceacb50867a4d46bc22e68076871
Markus Forbrig <markus.forbrig@mailbox.tu-dresden.de>
2021-02-12 10:49:55 +0100

    Update musicfeatureextractor.zsh to relative Path

--- ExtractionFSDKaggle2018/musicfeatureextractor.zsh
+++ ExtractionFSDKaggle2018/musicfeatureextractor.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # essentia audio feature extraction for AudioKP
-for file in /Users/homestudio/Downloads/EXTRACT_FSDKaggle2018.audio_train/*.wav
+for file in ../../EXTRACT_FSDKaggle2018.audio_train/*.wav
 do
   ./essentia_streaming_extractor_music "$file" "${file%.wav}.yaml" 
 done
