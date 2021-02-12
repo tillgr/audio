932589d91391b8d65c249729651e2c3db01bcbf3
Markus Forbrig <markus.forbrig@mailbox.tu-dresden.de>
2021-02-12 10:51:07 +0100

    Update musicfeatureextractor.zsh to json export

--- ExtractionFSDKaggle2018/musicfeatureextractor.zsh
+++ ExtractionFSDKaggle2018/musicfeatureextractor.zsh
@@ -2,5 +2,5 @@
 # essentia audio feature extraction for AudioKP
 for file in ../../EXTRACT_FSDKaggle2018.audio_train/*.wav
 do
-  ./essentia_streaming_extractor_music "$file" "${file%.wav}.yaml" 
+  ./essentia_streaming_extractor_music "$file" "${file%.wav}.json" 
 done
